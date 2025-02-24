2c025e2603e38d88539570cab64e00fb993d07ed
nfeds62 <nfeds62@gmail.com>
2025-02-23 23:04:10 -0600

    Implement FPS limiting using pygameClock

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+
+    # Initialize Pygame
+    pygame
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0  
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
     BLACK = (0, 0, 0)
@@ -16,9 +24,9 @@ def main():
          for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
-        screen.fill(BLACK)
-        pygame.display.flip()
+         screen.fill(BLACK)
+         dt = clock.tick(60) / 1000
+         pygame.display.flip()
 
 
 
